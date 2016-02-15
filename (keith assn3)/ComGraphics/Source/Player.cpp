e9d2912b1a0f313e8aff416aa3f5799cbc1cdef6
xSephiria <esrielthefallen@gmail.com>
2016-02-15 18:05:34 +0800

    Player.cpp & player.h updated

--- (keith assn3)/ComGraphics/Source/Player.cpp
+++ (keith assn3)/ComGraphics/Source/Player.cpp
@@ -1,10 +1,44 @@
 #include"Player.h"
 
-Player::Player(int hp)
+Player::Player(int hp) : HP(hp)
 {
 }
 
 Player::~Player()
 {
 
+}
+
+int Player::GetHp()
+{
+	return HP;
+}
+
+int Player::GetResources(int amount)
+{
+	Resources += amount;
+}
+
+float Player::WhileMining()
+{
+
+}
+void Player::TakeDmg(int dmg)
+{
+	HP -= dmg;
+}
+
+void Player::RecoverHp(int recovered)
+{
+	HP += recovered;
+}
+
+void Player::changeWeapon(char weapon)
+{
+	WeaponState = weapon;
+}
+
+char Player::getWeaponState()
+{
+	return WeaponState;
 }
--- (keith assn3)/ComGraphics/Source/Player.h
+++ (keith assn3)/ComGraphics/Source/Player.h
@@ -7,23 +7,21 @@ public:
 	Player(int hp);
 	~Player();
 	int GetHp();
-	int GetRecources();
+	int GetResources(int amount);
 
 	float WhileMining();	//increase and decrease the value for MiningAction
 	void TakeDmg(int dmg);
 	void RecoverHp(int recovered);
+	void changeWeapon(char weapon);
+	char getWeaponState();
 
-	int BulletsInClip;
-	int TotalBullets;
+	int TotalBullets; //Reloading
 	bool sprint;			//running or not
+	char WeaponState;		//which weapon u using
 private:
 	int HP;
 	int Resources;
 	float MiningAction;		//waving your pickaxe in da air
-	char WeaponState;		//which weapon u using
+	
 };
-
-
-
-
 #endif // !PLAYER_H
