dc8c804f0fecec15cd90afa72fba405e3d8000ab
prophet-leong <leongjunxiang1@hotmail.com>
2016-02-18 13:49:51 +0800

    Done turret targeting

--- (keith assn3)/ComGraphics/Source/Turret.cpp
+++ (keith assn3)/ComGraphics/Source/Turret.cpp
@@ -4,19 +4,55 @@
 Turret::Turret(const int&hp, const int&dmg, const Vector3 &pos) :damage(dmg),position(pos)
 {
 	Hp = hp;
+	fireDelay = 0;
+	shooting = 0;
 }
 
 Turret::~Turret()
 {
 }
-void Turret::nextTarget(Enemy enemy,Enemy nextEnemy)
+void Turret::nextTarget(Enemy enemy)
 {
-	if (enemy.isDead() == true)
+	Target = enemy.EnemyPos;
+	Vector3 view = Target - enemy.EnemyPos;
+	if (Target.z > enemy.EnemyPos.z){turretRotation = Math::RadianToDegree(atan(view.x / view.z)) - 180;}
+	else{ turretRotation = Math::RadianToDegree(atan(view.x / view.z)); }
+}
+float Turret::ShootAtEnemy(double dt)
+{
+	fireDelay += dt;
+	if (fireDelay < 3)
 	{
-		Target = nextEnemy.EnemyPos;
-		Vector3 view = Target - nextEnemy.EnemyPos;
-		if (Target.z > nextEnemy.EnemyPos.z){Math::RadianToDegree(atan(view.x / view.z)) - 180;}
-		else{Math::RadianToDegree(atan(view.x / view.z));}
+		if (shooting == true)
+		{
+			bulletPos += (Target - position)*BulletSpeed*dt;
+		}
+		else
+		{
+			shooting = true;
+			bulletPos = position;
+			Target = Alien;
+		}
+	}
+	else
+	{
+		shooting = false;
+		fireDelay = 0;
+	}
+}
+Vector3 Turret::TargetEnemy(Vector3 alien)
+{
+	if (shooting == true)
+	{
+		if (DoneTargeting == false)
+		{
+			Alien = alien;
+			DoneTargeting = true;
+		}
+	}
+	if (shooting == false)
+	{
+		DoneTargeting == false;
 	}
 }
 void Turret::ReduceHp(int dmg)
--- (keith assn3)/ComGraphics/Source/Turret.h
+++ (keith assn3)/ComGraphics/Source/Turret.h
@@ -9,14 +9,22 @@ public:
 	Turret(const int&hp, const int&dmg, const Vector3 &pos);
 	~Turret();
 
-	void nextTarget(Enemy enemy,Enemy nextEnemy);//*************Might Get Subscript error if not careful.
+	void nextTarget(Enemy enemy);//*************Might Get Subscript error if not careful.
 	void ReduceHp(int dmg);
-
+	float ShootAtEnemy(double dt);
+	float turretRotation;
+	Vector3 bulletPos;
+	Vector3 TargetEnemy(Vector3);
 private:
 	int Hp;
 	const int damage;
 	const Vector3 position;
 	Vector3 Target;
+	Vector3 Alien;
+	float fireDelay;
+	bool shooting;
+	float BulletSpeed;
+	bool DoneTargeting;
 };
 
 
